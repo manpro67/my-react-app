83b9bb9288d0536b6871089870dc6bc237ec821b
manoj <manoj2281998@gmail.com>
2024-06-15 18:14:01 +0530

    Initial commit

---
+++ src/MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function MyComponent() {
+    return (
+        <div>
+            <h2>This is my custom component</h2>
+            <p>Hello from MyComponent</p>
+        </div>
+    );
+}
+export default MyComponent;
